6b28e37a6abaf40a17dfd88a4f5c438ddee14ea0
cod1r <jasonho353@gmail.com>
2020-12-19 04:56:56 -0600

    optimized path finding algorithm from recursive to dijkstras

--- find_path_turtle.py
+++ find_path_turtle.py
@@ -5,14 +5,14 @@ def grid_make():
     A test function to generate a random grid.
     """
     grid = []
-    size_x = 300
-    size_y = 300
-    gap = 20
+    size_x = 1000
+    size_y = 1000
+    gap = 15
     for x in range(size_x//gap):
         row = []
         for y in range(size_y//gap):
             r = random.random()
-            if r <= .6:
+            if r <= .7:
                 row.append(0)
             else:
                 row.append(3)
@@ -26,8 +26,8 @@ def grid_make():
     indexes.remove(rt)
     ct = indexes[random.randrange(0, len(indexes))]
     indexes.remove(ct)
-    grid[cs][rs] = 1
-    grid[ct][rt] = 2
+    grid[rs][cs] = 1
+    grid[rt][ct] = 2
     # for x in grid:
     #     print(x)
     return grid
@@ -60,7 +60,7 @@ def calc(pA, pB):
     return (pA[0]-pB[0])**2+(pA[1]-pB[1])**2
 
 
-
+# needs optimizations badly. VERY BADLY
 def find_path(loc, path, been_to, d, grid, paths):
     """
     Recursively returns the paths to target location borrowing a simple heuristic from popular path finding algorithms which is the euclidean distance. 
@@ -101,15 +101,91 @@ def find_path(loc, path, been_to, d, grid, paths):
         for x in options:
             been_to_copy = been_to.copy()
             find_path(x.loc, path[:], been_to_copy, d, grid, paths)
-                
+
+
+class vertex:
+    def __init__(self, weight, loc):
+        self.weight = weight
+        self.loc = loc
+        self.visited = False
+
+def Dijkstras(grid, info):
+    nodes = [ [ vertex(float('inf'), (r, c)) for c in range(len(grid[r])) ] for r in range(len(grid)) ]
+    path = {}
+    vertexes = []
+    for _ in nodes:
+        for v in _:
+            path[v.loc] = []
+            vertexes.append(v)
+
+    vertexes[(info['s'][0])*len(grid)+(info['s'][1])].weight = 0
+
+    # print("start", vertexes[(info['s'][0])*len(grid)+(info['s'][1])].loc)
+    # print("length of list", len(vertexes))
+    while True:
+        # print(list(map(lambda x: x.loc, path)))
+        _min = vertex(float('inf'), None)
+        i = 0
+        for v in range(len(vertexes)):
+            if _min.weight > vertexes[v].weight:
+                _min = vertexes[v]
+                i = v
+        
+        
+        if (_min.loc == info['t']):
+            print("DONE")
+            break
+
+        if not (_min.loc[0] + 1 >= len(grid)) and not vertexes[(_min.loc[0] + 1)*len(grid) + (_min.loc[1])].visited and vertexes[(_min.loc[0] + 1)*len(grid) + (_min.loc[1])].loc not in info['w']:
+
+            # print(1, vertexes[(_min.loc[0] + 1)*len(grid) + (_min.loc[1])].loc)
+            path[vertexes[(_min.loc[0] + 1)*len(grid) + (_min.loc[1])].loc].append(_min.loc)
+            vertexes[(_min.loc[0] + 1)*len(grid) + (_min.loc[1])].weight = min(vertexes[(_min.loc[0] + 1)*len(grid) + (_min.loc[1])].weight, _min.weight + calc(info['s'], (_min.loc[0] + 1, _min.loc[1])))
+
+        if not (_min.loc[0] - 1 < 0) and not vertexes[(_min.loc[0] - 1)*len(grid) + (_min.loc[1])].visited and vertexes[(_min.loc[0] - 1)*len(grid) + (_min.loc[1])].loc not in info['w']:
+
+            # print(2, vertexes[(_min.loc[0] - 1)*len(grid) + (_min.loc[1])].loc)
+            path[vertexes[(_min.loc[0] - 1)*len(grid) + (_min.loc[1])].loc].append(_min.loc)
+            vertexes[(_min.loc[0] - 1)*len(grid) + (_min.loc[1])].weight = min(vertexes[(_min.loc[0] - 1)*len(grid) + (_min.loc[1])].weight, _min.weight + calc(info['s'], (_min.loc[0] - 1, _min.loc[1])))
+
+        if not (_min.loc[1] + 1 >= len(grid[0])) and not vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] + 1)].visited and vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] + 1)].loc not in info['w']:
+
+            # print(3, vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] + 1)].loc)
+            path[vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] + 1)].loc].append(_min.loc)
+            vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] + 1)].weight = min(vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] + 1)].weight, _min.weight + calc(info['s'], (_min.loc[0], _min.loc[1] + 1)))
+        
+        if not (_min.loc[1] - 1 < 0) and not vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] - 1)].visited and vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] - 1)].loc not in info['w']:
+
+            # print(4, vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] - 1)].loc)
+            path[vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] - 1)].loc].append(_min.loc)
+            vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] - 1)].weight = min(vertexes[(_min.loc[0])*len(grid) + (_min.loc[1] - 1)].weight, _min.weight + calc(info['s'], (_min.loc[0], _min.loc[1] - 1)))
+
+        vertexes[i].weight = float('inf')
+        vertexes[i].visited = True
+
+    journey = [info['t']]
+    curr = path[info['t']][0]
+    while True:
+        if curr == info['s']:
+            journey.append(curr)
+            break
+        journey.append(curr)
+        curr = path[curr][0]
+
+    return journey[::-1]
+        
+            
+    
 
 if __name__ == "__main__":
     grid = grid_make()
+    print(len(grid), len(grid[0]))
     for x in grid:
         print(x)
     d = establish_knowns(grid)
     print("start: ", d['s'], "end: ", d['t'])
-    paths = set()
-    find_path(d['s'], [], set(), d, grid, paths)
-    for x in paths:
-        print(x)
+    # paths = set()
+    # been_to = set()
+    # find_path(d['s'], [], been_to, d, grid, paths)
+    paths = Dijkstras(grid, d)
+    print(paths)
--- generate_grid.py
+++ generate_grid.py
@@ -8,32 +8,23 @@ import find_path_turtle
 
 root = tkinter.Tk()
 root.resizable(False, False)
-size_x = 200
-size_y = 200
-gap = 20
+size_x = 1000
+size_y = 1000
+gap = 15
 root.geometry(str(size_x+10)+'x'+str(size_y+10))
 canvas = tkinter.Canvas(root, width=size_x, height=size_y)
 t = turtle.TurtleScreen(canvas)
 t.setworldcoordinates(0, size_y, size_x, 0)
-for x in range(50):
+for x in range(500):
     canvas.create_line(x*gap, 0, x*gap, size_y)
-for y in range(50):
+for y in range(500):
     canvas.create_line(0, y*gap, size_x, y*gap)
 
 
 # randomly generates a grid like maze
 
-grid = []
-for x in range(size_x//gap):
-    row = []
-    for y in range(size_y//gap):
-        r = random.random()
-        if r <= .8:
-            row.append(0)
-        else:
-            row.append(3)
-    grid.append(row)
-
+grid = find_path_turtle.grid_make()
+d = find_path_turtle.establish_knowns(grid)
 
 # loops through grid to see where to create a black square
 
@@ -43,31 +34,14 @@ for y in range(len(grid)):
         if grid[y][x] == 3:
             canvas.create_rectangle((x*gap, y*gap, (x*gap)+gap, (y*gap)+gap), fill='black')
 
-
-# randomly gets the indexes from a list so that copies of the same coordinates are not possible
-
-indexes = [x for x in range(size_x//gap)]
-rs = indexes[random.randrange(0, len(indexes))]
-indexes.remove(rs)
-cs = indexes[random.randrange(0, len(indexes))]
-indexes.remove(cs)
-rt = indexes[random.randrange(0, len(indexes))]
-indexes.remove(rt)
-ct = indexes[random.randrange(0, len(indexes))]
-indexes.remove(ct)
-
-# randomly generates where the turtle will start and where it will go to.
-
-grid[cs][rs] = 1
-grid[ct][rt] = 2
-canvas.create_rectangle((rs*gap, cs*gap, (rs*gap)+gap, (cs*gap)+gap), fill='green')
-canvas.create_rectangle((rt*gap, ct*gap, (rt*gap)+gap, (ct*gap)+gap), fill='red')
+canvas.create_rectangle((d['s'][1]*gap, d['s'][0]*gap, (d['s'][1]*gap)+gap, (d['s'][0]*gap)+gap), fill='green')
+canvas.create_rectangle((d['t'][1]*gap, d['t'][0]*gap, (d['t'][1]*gap)+gap, (d['t'][0]*gap)+gap), fill='red')
 
 # sets the turtles position.
 
 tt = turtle.RawTurtle(t)
 tt.penup()
-tt.setpos((rs*gap+gap//2, cs*gap+gap//2))
+tt.setpos((d['s'][1]*gap+gap//2, d['s'][0]*gap+gap//2))
 tt.pendown()
 
 # some settings that have to be set that lets the gui be seen correctly. Some are required some are not.
@@ -75,16 +49,15 @@ tt.pendown()
 canvas.grid(column=0, row=0)
 canvas.bind()
 canvas.pack()
-tt._delay(20)
 
 
 def get_grid_and_solve():
     # puts the location of the black squares, start, and stop positions in a dictionary
     d = find_path_turtle.establish_knowns(grid)
     # list to contain all the possible paths to target location.
-    paths = set()
+    # paths = set()
     # function that recursively finds all the possible paths to location.
-    find_path_turtle.find_path(d['s'], [], set(), d, grid, paths)
+    # find_path_turtle.find_path(d['s'], [], set(), d, grid, paths)
     # sorts the paths list based on length
     # paths.sort(key=len)
     # loops through all the paths
@@ -93,10 +66,10 @@ def get_grid_and_solve():
         # tt.penup()
         # tt.setpos((rs*gap+gap//2, cs*gap+gap//2))
         # tt.pendown()
-    x = min(paths, key=len)
+    # x = min(paths, key=len)
+    x = find_path_turtle.Dijkstras(grid, d)
     # previous coordinate so that it knows how to turn
     prev = x[0]
-
     for p in x:
         # conditions that turn the turtle
         if p[1] > prev[1]:
@@ -111,6 +84,8 @@ def get_grid_and_solve():
         tt.goto(p[1]*gap+gap//2, p[0]*gap+gap//2)
         prev = p
     # calls the gui event loop to show the events
+    if tt.pos()[0] == (d['t'][1]*gap)+gap//2 and tt.pos()[1] == (d['t'][0]*gap)+gap//2:
+        root.after(2000, lambda: root.destroy())
     t.mainloop()
 
 
